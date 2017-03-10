86f5ad7072cfc9f7aa5151ecbaab4653ece5571b
SalvaG <sgregori@users.noreply.github.com>
2017-03-10 16:15:10 +0100

    Update roundcube_contacts_export_curl.sh

--- roundcube_contacts_export_curl.sh
+++ roundcube_contacts_export_curl.sh
@@ -10,6 +10,6 @@ curl -L -b cookies -c cookies -X POST -e $url -F"_action=login" -F"_pass=$PASS" 
 
 token=curl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -e $url $url |grep "name=\"_token" |sed 's/.*value="\([^"]*\)".*/\1/'
 
-curl -o contacts.vcf -ssl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -F"_source=0" -F"_token=$token" -s "https://serviciodecorreo.es/?_task=addressbook&_source=0&_action=export" > /dev/null
+curl -o contacts.vcf -ssl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -F"_source=0" -F"_token=$token" -s "$url/?_task=addressbook&_source=0&_action=export" > /dev/null
 
 cat contacts.vcf
