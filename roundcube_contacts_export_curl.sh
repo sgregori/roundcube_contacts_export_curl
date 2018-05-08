cafb63378470333efb0b43a25472a6e3be69339d
SalvaG <sgregori@users.noreply.github.com>
2018-05-08 12:54:58 +0200

    Fix para roundcube plus de arsys

--- roundcube_contacts_export_curl.sh
+++ roundcube_contacts_export_curl.sh
@@ -8,8 +8,8 @@ token=`curl -c cookies -X GET $url |grep "name=\"_token" |sed 's/.*value="\([^"]
 
 curl -L -b cookies -c cookies -X POST -e $url -F"_action=login" -F"_pass=$PASS" -F"_task=login" -F"_timezone=Europe/Berlin" -F"_token=$token" -F"_url=" -F"_user=$user" $url > /dev/null
 
-token=`curl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -e $url $url |grep "name=\"_token" |sed 's/.*value="\([^"]*\)".*/\1/'`
+result=`curl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -e $url $url | grep "name=\"_token" |grep -Po 'name="_token" value="\K[[:word:]]*'`
 
-curl -o contacts.vcf -ssl -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -F"_source=0" -F"_token=$token" -s "$url/?_task=addressbook&_source=0&_action=export" > /dev/null
+curl -o contacts.vcf -L -b cookies -c cookies -X GET -F"_task=addressbook" -F"_action=export" -F"_source=0" -F"_token=$result" -e $url -s "$url/?_task=addressbook&_source=0&_action=export&_token=$result" > /dev/null
 
 cat contacts.vcf
